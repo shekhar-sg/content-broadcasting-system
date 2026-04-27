a9562a97ae80971a408cf370a1c4029d68950451
SHEKHAR GUPTA <137384631+shekhar-sg@users.noreply.github.com>
2026-04-27 23:51:27 +0530

    update API routing and enhance server initialization

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,19 @@
-import bootstrap from '../src/main'
+import type { Request, Response } from "express";
+import bootstrap from "../src/main";
 
-const app = bootstrap().catch((error) => {
+let appPromise: ReturnType<typeof bootstrap>;
+
+async function handler(req: Request, res: Response) {
+  try {
+    if (!appPromise) {
+      appPromise = bootstrap();
+    }
+    const app = await appPromise;
+    app(req, res);
+  } catch (error) {
     console.error("Failed to start server:", error);
     process.exit(1);
-})
+  }
+}
 
-export default app
+export default handler;
